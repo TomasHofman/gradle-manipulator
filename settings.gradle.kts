809a226caf289b52eb9a0623e8df2e0dde1bc94e
Tomas Hofman <thofman@redhat.com>
2019-05-15 16:40:22 +0200

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,13 @@
-/*pluginManagement {
+pluginManagement {
     repositories {
         maven {
             url = uri("https://plugins.gradle.org/m2/")
         }
+        maven {
+            url = uri("https://indy-proxy.newcastle-stage.svc.cluster.local/m2/")
+        }
     }
-}*/
+}
 rootProject.name = "gradle-manipulator"
 include("analyzer")
 include("manipulation")
