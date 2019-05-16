907e886a7c6be1794cca876e50d71b6fc5b25aaf
Tomas Hofman <thofman@redhat.com>
2019-05-16 14:44:13 +0200

    test

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,10 @@
-/*pluginManagement {
+pluginManagement {
     repositories {
         maven {
             url = uri("https://plugins.gradle.org/m2/")
         }
         maven {
-            url = uri(System.getenv("https_proxy") + "/m2/")
-        }
-    }
-}*/
-pluginManagement {
-    repositories {
-        maven {
-            url = uri(System.getProperty("maven.repo.url"))
+            url = uri(System.getenv("AProxDependencyUrl"))
         }
     }
 }
